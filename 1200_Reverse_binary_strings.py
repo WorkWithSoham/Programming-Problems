be29a5c0f57343ea922c788be387c58425b57027
WorkWithSoham <sohamtembe98@gmail.com>
2021-03-24 00:41:32 +0530

    Update 1200_Reverse_binary_strings.py

--- 1200_Reverse_binary_strings.py
+++ 1200_Reverse_binary_strings.py
@@ -1,9 +1,18 @@
+import math
+
 tc = int(input())
 while tc:
 
-    
+    length = input()
+    string = input()
+    chars = [x for x in string]
 
+    count = 0
+    # count_max = 0
+    for x in range(len(chars)-1):
+        if chars[x] == chars[x+1]:
+            count += 1
 
-
+    print(math.ceil(count/2))
 
     tc-=1
