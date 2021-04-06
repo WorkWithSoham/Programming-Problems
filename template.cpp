8de7f69609576868c37cfb7282c2d696d77a0274
WorkWithSoham <sohamtembe98@gmail.com>
2021-04-06 22:59:22 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+#define fo(n) for(int i=0; i<n; i++)
+#define vi vector<int>
+#define ll long long
+
+int main(){
+    
+    return 0;
+}
