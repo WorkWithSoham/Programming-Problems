15b6127c09b535d1a33d85021d3fd83d7770bea2
WorkWithSoham <sohamtembe98@gmail.com>
2021-04-07 21:46:07 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 #define fo(a, n) for(int i=a; i<n; i++)
-#define fo1(a, n) for(int i=a; i>=n; i--)
+#define fo1(a, n) for(int i=a; i>n; i--) // 
 #define vi vector<int>
 #define ll long long
 
